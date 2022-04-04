a80e18235659116dee77ce789a2ab027aa0a434b
Josh Booth <boothjmail@gmail.com>
2022-04-04 09:37:31 -0400

    fixed interrupt builk

--- BattleBot/firmware/src/bt.c
+++ BattleBot/firmware/src/bt.c
@@ -3,6 +3,7 @@
 #include "motor.h"
 
 #define QUEUE_LEN 15
+#define L_BIAS_VALUE 10
 
 char command[3];
 
@@ -61,8 +62,8 @@ void button_handler(char button){
 }
 
 
-
-void switch_handler(char side, char magnitude){
+// for L & R independent controls
+void switch_handler_independent(char side, char magnitude){
     int mag_int = (int)magnitude-'0';
     int direction = (mag_int<4) ? -1 : 1;
     int speed = (mag_int>4) ? mag_int-4 :
@@ -83,13 +84,34 @@ void switch_handler(char side, char magnitude){
     }
 }
 
+void switch_handler_straight(char side, char magnitude){
+    int mag_int = (int)magnitude-'0';
+    int direction = (mag_int<4) ? -1 : 1;
+    int speed = (mag_int>4) ? mag_int-4 :
+                (mag_int==3) ? 1 :
+                (mag_int==2) ? 2 :
+                (mag_int==1) ? 3 :
+                (mag_int==0) ? 4 : 0;
+  
+    switch(side){
+        case 'L': // straight / backwards
+            movement_set_direction_left(direction);
+            movement_set_direction_right(direction);
+            movement_set_speed_left(speed + L_BIAS_VALUE);
+            movement_set_speed_right(speed);
+            break;
+        case 'R':
+
+            break;
+    }
+}
 void parse_command(void){
     switch(command[0]){
         case 'B':
             button_handler(command[1]);
             break;
         case 'S':
-            switch_handler(command[1], command[2]);
+            switch_handler_independent(command[1], command[2]);
             break;
     }
 }
